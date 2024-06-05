4593503c137582f1866813269d3a6ff567ad4c62
Simon Zargarian <simonzargariancv@gmail.com>
2024-06-05 12:26:00 +0200

    Initial commit

--- src/components/home/Experience/data.tsx
+++ src/components/home/Experience/data.tsx
@@ -26,7 +26,7 @@ export const experienceList:[Experiences] = [
                 name: "XLNT Group",
                 detail: "As a developer and AI Manager at the helm of the automation project, my role encompassed the end-to-end development and integration of advanced AI models and robust software solutions.  My responsibilities were multi-faceted, involving deep research as a data scientist  into existing processes and data, designing Java, JavaScript and AI based algorithms such as OpenAI Gpt, Google Gemma and Facebook Llama ",
                 tech: ["Google Gemma", "Open-AI GTP", "React", "TenserFlow"],
-                link: "https://junkielabs.in/",
+                link: "#contained-button",
                 image: xlnt
             },
             {
@@ -34,7 +34,7 @@ export const experienceList:[Experiences] = [
                 name: "Devies",
                 detail: "The client required an e-commerce platform to sell commodities online that was not only efficient, but also able to handle large amounts of traffic and transactions while providing a smooth user experience. The platform had to be able to integrate with various payment gateways, shipping providers, and tax calculators. ",
                 tech: ["Java", "React", "Spring Boot", "TypeScript" ],
-                link: "https://junkielabs.in/",
+                link: "#contained-button",
                 image: devies
             },
             {
@@ -42,6 +42,7 @@ export const experienceList:[Experiences] = [
                 name: "Leo Vegas",
                 detail: "Working within the Responsible Gaming department, I was part of a team that ensured business logic was set in place that enforced that compliance and mandatory requirements were being upheld. I managed the entire lifecycle process of the assignments I was handed, starting from understanding the business aspect, developing the logic, unit testing, integration testing, manual testing, release and deployment on multiple markets",
                 tech: ["GCP", "MySql","Spring Boot", "Kotlin", "Java"],
+                link: "#contained-button",
                 image: leo
             }
         ]
